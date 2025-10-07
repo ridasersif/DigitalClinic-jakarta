d3c89ab774de73a16c5de216b884abcdc06e4776
ridasersif <ridasersif1@gmail.com>
2025-10-07 11:50:14 +0200

    Add Role entity with role name and relation to User

--- project/src/main/java/ma/clinique/project/models/Role.java
+++ project/src/main/java/ma/clinique/project/models/Role.java
@@ -1,4 +1,49 @@
 package ma.clinique.project.models;
 
+import jakarta.persistence.*;
+import ma.clinique.project.models.enums.RoleType;
+
+@Entity
+@Table(name = "roles")
+
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role_name", nullable = false, unique = true, length = 50)
+    private RoleType roleName;
+
+    // Constructeurs
+    public Role() {}
+
+    public Role(RoleType roleName) {
+        this.roleName = roleName;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public RoleType getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(RoleType roleName) {
+        this.roleName = roleName;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", roleName=" + roleName +
+                '}';
+    }
 }
