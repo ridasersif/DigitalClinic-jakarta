a5f8dea3e115f35e98baa294b23a1cadd26a08d0
ridasersif <ridasersif1@gmail.com>
2025-10-07 11:50:13 +0200

    Add Admin entity extending User with admin-specific data

--- project/src/main/java/ma/clinique/project/models/Admin.java
+++ project/src/main/java/ma/clinique/project/models/Admin.java
@@ -1,4 +1,23 @@
 package ma.clinique.project.models;
 
-public class Admin {
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "admins")
+@PrimaryKeyJoinColumn(name = "user_id")
+public class Admin extends User {
+
+    // Constructeurs
+    public Admin() {
+        super();
+    }
+
+    public Admin(String firstName, String lastName, String email, String password, Role role) {
+        super(firstName, lastName, email, password, role);
+    }
+
+    @Override
+    public String toString() {
+        return "Admin{id=" + getId() + ", name='" + getFullName() + "'}";
+    }
 }
