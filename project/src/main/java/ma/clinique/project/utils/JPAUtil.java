a5102a47139199f8e5114cdfc2252dd81cd55d76
ridasersif <ridasersif1@gmail.com>
2025-10-07 11:50:12 +0200

    Add JPAUtil class to manage EntityManagerFactory

--- project/src/main/java/ma/clinique/project/utils/JPAUtil.java
+++ project/src/main/java/ma/clinique/project/utils/JPAUtil.java
@@ -1,4 +1,20 @@
 package ma.clinique.project.utils;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class JPAUtil {
+    private static final EntityManagerFactory emf =
+            Persistence.createEntityManagerFactory("CliniqueDigitalePU");
+
+    public static EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public static void close() {
+        if (emf != null && emf.isOpen()) {
+            emf.close();
+        }
+    }
 }
