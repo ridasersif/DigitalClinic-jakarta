c00144d22a5a6099b7e9210a1366f9a2c0ced124
ridasersif <ridasersif1@gmail.com>
2025-10-07 11:50:13 +0200

    Add Department entity with doctor list relation

--- project/src/main/java/ma/clinique/project/models/Department.java
+++ project/src/main/java/ma/clinique/project/models/Department.java
@@ -1,4 +1,67 @@
+// Department.java
 package ma.clinique.project.models;
 
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "departments")
 public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String name;
+
+    @OneToMany(mappedBy = "department", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Doctor> doctors = new ArrayList<>();
+
+    // Constructeurs
+    public Department() {}
+
+    public Department(String name) {
+        this.name = name;
+    }
+
+    // Getters et Setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Doctor> getDoctors() {
+        return doctors;
+    }
+
+    public void setDoctors(List<Doctor> doctors) {
+        this.doctors = doctors;
+    }
+
+    public void addDoctor(Doctor doctor) {
+        doctors.add(doctor);
+        doctor.setDepartment(this);
+    }
+
+    public void removeDoctor(Doctor doctor) {
+        doctors.remove(doctor);
+        doctor.setDepartment(null);
+    }
+
+    @Override
+    public String toString() {
+        return "Department{id=" + id + ", name='" + name + "', doctorsCount=" + doctors.size() + "}";
+    }
 }
