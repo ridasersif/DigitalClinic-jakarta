e20d705efcd2b077143899bdbfc397e9f63849ea
ridasersif <ridasersif1@gmail.com>
2025-10-14 16:49:53 +0200

    Add AdminDashboardServlet to handle admin dashboard requests

---
+++ project/src/main/java/ma/clinique/project/controller/admin/AdminDashboardServlet.java
@@ -0,0 +1,20 @@
+package ma.clinique.project.controller.admin;
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/admin/dashboard")
+public class AdminDashboardServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/views/admin/dashboard.jsp").forward(request, response);
+    }
+}
