de3852bf082ccc42771ab3f79adfc5e8ab48ab79
ridasersif <ridasersif1@gmail.com>
2025-10-14 17:53:45 +0200

    Format JPAUtil class for improved readability

--- project/src/main/java/ma/clinique/project/utils/JPAUtil.java
+++ project/src/main/java/ma/clinique/project/utils/JPAUtil.java
@@ -7,14 +7,17 @@ import jakarta.persistence.Persistence;
 public class JPAUtil {
     private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("CliniqueDigitalePU");
 
-    public static EntityManager getEntityManager() {
 
+    public static EntityManager getEntityManager() {
         return emf.createEntityManager();
     }
+
 
     public static void close() {
         if (emf != null && emf.isOpen()) {
             emf.close();
         }
     }
+
+
 }
