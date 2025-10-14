ae5dd638a278869a40d0231a1f17d0cea4fd91ec
ridasersif <ridasersif1@gmail.com>
2025-10-14 16:53:25 +0200

    Add LogoutServlet to handle user logout functionality

---
+++ project/src/main/java/ma/clinique/project/controller/auth/LogoutServlet.java
@@ -0,0 +1,21 @@
+package ma.clinique.project.controller.auth;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/auth/logout")
+public class LogoutServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getSession().invalidate();
+
+        response.sendRedirect(request.getContextPath() + "/");
+    }
+}
