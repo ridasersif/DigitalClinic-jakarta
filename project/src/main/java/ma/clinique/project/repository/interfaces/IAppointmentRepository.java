f61a4bb5a123978e213e62c7f20cd4fde084ecad
ridasersif <ridasersif1@gmail.com>
2025-10-16 21:15:05 +0200

    Add appointment retrieval methods and cancellation functionality to IAppointmentRepository

--- project/src/main/java/ma/clinique/project/repository/interfaces/IAppointmentRepository.java
+++ project/src/main/java/ma/clinique/project/repository/interfaces/IAppointmentRepository.java
@@ -2,6 +2,7 @@ package ma.clinique.project.repository.interfaces;
 
 import ma.clinique.project.models.Appointment;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface IAppointmentRepository {
@@ -10,5 +11,12 @@ public interface IAppointmentRepository {
         boolean delete(Integer id);
         Appointment findById(Integer id);
         List<Appointment> findAll();
+        List<Appointment> findByDoctorAndDate(Integer doctorId, LocalDate date);
+        List<Appointment> findByRoomAndDate(Integer roomId, LocalDate date);
+        List<Appointment> findByPatientAndDate(Integer patientId, LocalDate date);
+        List<Appointment> findAllAppointmentsByPatientId(Integer patientId);
+        List<Appointment> findAllAppointmentsByDoctorId(Integer doctorId);
+        void cancelAppointment(Integer id);
+
 
 }
